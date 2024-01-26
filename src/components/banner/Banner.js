7df9ed9d3e3eddc398b9ab4f0decd849d09d1017
aagneye-syam <saagneye2003@gmail.com>
2024-01-26 21:25:06 +0530

    description and fade_bottom for banner added

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -4,13 +4,16 @@ import './Banner.css'
 function Banner() {
   return (
     <div className='banner'>
-      <div className="content">
-        <h1 classname="title">Movie name</h1>
-        <div className="banner_buttons">
+      <div className='content'>
+        <h1 className='title'>Movie name</h1>
+        <div className='banner_buttons'>
                 <button className='button'>Play</button>
                 <button className='button'>My list</button>
         </div>
-        <h1 className="description">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document</h1>
+        <h1 className='description'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document</h1>
+      </div>
+      <div className="fade_bottom">
+
       </div>
     </div>
   )
