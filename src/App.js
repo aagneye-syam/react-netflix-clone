1c5653f9998697b2af23bbd6f8e7c5492bf1f880
aagneye-syam <saagneye2003@gmail.com>
2024-01-26 18:11:48 +0530

    banner added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css"
 import Navbar from "./components/navbar/Navbar";
+import Banner from "./components/banner/Banner";
 
 function App() {
   return (
     <div className="App">
          <Navbar />
+         <Banner />
     </div>
   );
 }
---
+++ src/components/banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='banner'>
+      <div className="content">
+        <h1 classname="title">Movie name</h1>
+        <div className="banner_buttons">
+                <button className='button'>Play</button>
+                <button className='button'>My list</button>
+        </div>
+        <h1 className="description">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
