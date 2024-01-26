1b95728629a3cb59c57e1689b600b0fd188cd4c9
aagneye-syam <saagneye2003@gmail.com>
2024-01-26 21:40:54 +0530

    row post added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css"
 import Navbar from "./components/navbar/Navbar";
 import Banner from "./components/banner/Banner";
+import RowPost from "./components/rowPost/RowPost";
 
 function App() {
   return (
     <div className="App">
          <Navbar />
          <Banner />
+         <RowPost />
     </div>
   );
 }
