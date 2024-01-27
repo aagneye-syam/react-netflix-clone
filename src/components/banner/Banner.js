5bffa85f5d74b9d3c238e6a3b6603e4eb21a2bea
aagneye-syam <saagneye2003@gmail.com>
2024-01-27 21:28:40 +0530

    constants and axios instance created

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Banner.css'
 
 function Banner() {
+  
+
   return (
     <div className='banner'>
       <div className='content'>
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+
+export const API_KEY =  '408e06deb50b9240ce511d670c019887';
