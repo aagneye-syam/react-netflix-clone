de3c95e979ea0c6ba153a14b12100807992f4a77
aagneye-syam <saagneye2003@gmail.com>
2024-01-27 23:43:04 +0530

    useEffect & axios added in RowPost.js

--- src/components/rowPost/RowPost.js
+++ src/components/rowPost/RowPost.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './RowPost.css'
+import axios from '../../axios';
+import { API_KEY } from '../../constants/Constants';
 
 function RowPost() {
+  useEffect(() => {
+    axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then((response)=>{
+      console.log(response.data)
+    }).catch(err=>{alert('Network error')})
+
+    }, [])
+    
   return (
-    <div className='row'>
+
+     <div className='row'>
       <h2>Title</h2>
       <div className="posters">
         <img className='poster' src="https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w" alt="Poster" />
