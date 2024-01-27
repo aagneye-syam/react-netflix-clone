f198a1d45cc21844fa620be0dcd180674bbcb6d0
aagneye-syam <saagneye2003@gmail.com>
2024-01-27 22:05:35 +0530

    movie description added with ternary operation

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -23,10 +23,7 @@ function Banner() {
           <button className="button">Play</button>
           <button className="button">My list</button>
         </div>
-        <h1 className="description">
-          In publishing and graphic design, Lorem ipsum is a placeholder text
-          commonly used to demonstrate the visual form of a document
-        </h1>
+        <h1 className="description">{movie ? movie.overview:""}</h1>
       </div>
       <div className="fade_bottom"></div>
     </div>
