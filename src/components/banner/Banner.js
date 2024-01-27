05ab503e402f5bbee53f3c051af8ab834b57e72c
aagneye-syam <saagneye2003@gmail.com>
2024-01-27 21:56:06 +0530

    useState and useEffect added

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,24 +1,36 @@
-import React, { useEffect } from 'react'
-import './Banner.css'
+import React, { useEffect, useState } from "react";
+import { API_KEY } from "../../constants/constants";
+import "./Banner.css";
+import axios from "../../axios";
 
 function Banner() {
-  
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response) => {
+        console.log(response.data.results[0]);
+        setMovie(response.data.results[0])
+      });
+
+    return () => {};
+  }, []);
 
   return (
-    <div className='banner'>
-      <div className='content'>
-        <h1 className='title'>Movie name</h1>
-        <div className='banner_buttons'>
-                <button className='button'>Play</button>
-                <button className='button'>My list</button>
+    <div className="banner">
+      <div className="content">
+        <h1 className="title">{movie.title}</h1>
+        <div className="banner_buttons">
+          <button className="button">Play</button>
+          <button className="button">My list</button>
         </div>
-        <h1 className='description'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document</h1>
+        <h1 className="description">
+          In publishing and graphic design, Lorem ipsum is a placeholder text
+          commonly used to demonstrate the visual form of a document
+        </h1>
       </div>
-      <div className="fade_bottom">
-
-      </div>
+      <div className="fade_bottom"></div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
