fd20f25010b7b873f25f3d631aeabe77a70686ed
aagneye-syam <saagneye2003@gmail.com>
2024-01-28 09:24:48 +0530

    checking response array is empty or not

--- src/components/rowPost/RowPost.js
+++ src/components/rowPost/RowPost.js
@@ -6,43 +6,50 @@ import YouTube from "react-youtube";
 
 function RowPost(props) {
   const [movies, setMovie] = useState([]);
-  
+  const [urlId, setUrlId] = useState([]);
+
   useEffect(() => {
-    axios.get(props.url).then((response) => {
-        setMovie(response.data.results)
+    axios
+      .get(props.url)
+      .then((response) => {
+        setMovie(response.data.results);
       })
       .catch((err) => {
-       // alert("Network error");
+        // alert("Network error");
       });
-  }, []);  
-  
-    const opts = {
-    height: '390',
-    width: '100%',
+  }, []);
+
+  const opts = {
+    height: "390",
+    width: "100%",
     playerVars: {
       autoplay: 1,
     },
   };
 
-  const handleMoveTrailer = (id)=>{
-    console.log(id) 
-    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-Us`).then((response)=>{console.log(response.data)})
-  }
-
-  return (
-    <div className="row">
-      <h2>{props.title}</h2>
-      <div className="posters">
-        {
-          movies.map((obj)=>
-          <img onClick={()=>handleMoveTrailer(obj.id)} className={props.isSmall ? "small_poster" : "poster"} alt="Poster" src={`${imageUrl+obj.backdrop_path}`}  />
-
-          )
+  const handleMoveTrailer = (id) => {
+    console.log(id);
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-Us`).then((response) => {
+        if (response.data.results.length !== 0) {
+          setUrlId(response.data.results[0]);
+        }else{
+          console.log(`Array empty`)
         }
+      });
+
+    return (
+      <div className="row">
+        <h2>{props.title}</h2>
+        <div className="posters">
+          {movies.map((obj) => (
+            <img
+              onClick={() => handleMoveTrailer(obj.id)} className={props.isSmall ? "small_poster" : "poster"} alt="Poster" src={`${imageUrl + obj.backdrop_path}`} />
+          ))}
+        </div>
+        { <YouTube opts={opts} videoId={urlId.key} />}
       </div>
-      <YouTube opts={opts} videoId="2g811Eo7K8U" />
-    </div>
-  );
+    );
+  };
 }
 
 export default RowPost;
