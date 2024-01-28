138cad5c4b9a12700324e37e1d25ed2050d70c10
aagneye-syam <saagneye2003@gmail.com>
2024-01-28 09:50:51 +0530

    other genre added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css"
 import Navbar from "./components/navbar/Navbar";
 import Banner from "./components/banner/Banner";
 import RowPost from "./components/rowPost/RowPost";
-import { originals,action } from "./urls";
+import { originals,action, horror,trending,comedy,romance} from "./urls";
 
 function App() {
   return (
@@ -12,7 +12,10 @@ function App() {
          <Banner />
          <RowPost url={originals} title='Netflix Orginals' />
          <RowPost url={action} title='Action' isSmall />
-         
+         <RowPost url={trending} title='Trending' isSmall />\
+         <RowPost url={horror} title='Horror' isSmall />
+         <RowPost url={comedy} title='Comedy' isSmall />
+         <RowPost url={romance} title='Romance' isSmall />
     </div>
   );
 }
