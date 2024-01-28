e743167edcf0f6377b0b6023caf10f6904f1a9e1
aagneye-syam <saagneye2003@gmail.com>
2024-01-28 09:09:36 +0530

    onClick youtube video boc added

--- src/components/rowPost/RowPost.js
+++ src/components/rowPost/RowPost.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./RowPost.css";
 import axios from "../../axios";
-import { imageUrl } from "../../constants/Constants";
+import { API_KEY, imageUrl } from "../../constants/Constants";
+import YouTube from "react-youtube";
 
 function RowPost(props) {
   const [movies, setMovie] = useState([]);
+  
   useEffect(() => {
     axios.get(props.url).then((response) => {
         setMovie(response.data.results)
@@ -12,7 +14,16 @@ function RowPost(props) {
       .catch((err) => {
        // alert("Network error");
       });
-  }, []);
+  }, []);  
+  
+    const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
 
   return (
     <div className="row">
@@ -20,11 +31,12 @@ function RowPost(props) {
       <div className="posters">
         {
           movies.map((obj)=>
-          <img className={props.isSmall ? "small_poster" : "poster"} alt="Poster" src={`${imageUrl+obj.backdrop_path}`}  />
+          <img onClick={()=>handleMoveTrailer(obj.id)} className={props.isSmall ? "small_poster" : "poster"} alt="Poster" src={`${imageUrl+obj.backdrop_path}`}  />
 
           )
         }
       </div>
+      <YouTube opts={opts} videoId="2g811Eo7K8U" />
     </div>
   );
 }
